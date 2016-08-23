aa9887580b5409187c94bf242f9c4900ed7123e4
Tal Rahamim <Tal Rahamim>
2016-08-23 21:17:29 +0300

    Mazal Tov - First Commit!

---
+++ app/src/main/java/tal/tal/gestures_accessible_keyboard/MainIME.java
@@ -0,0 +1,10 @@
+package tal.tal.gestures_accessible_keyboard;
+
+import android.inputmethodservice.InputMethodService;
+
+/**
+ * Created by talra on 23-Aug-16.
+ */
+public class MainIME extends InputMethodService
+{
+}
